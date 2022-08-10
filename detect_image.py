0c31fc69c0de356aa3e752ad24f6d6d0a67be3ff
Minksyn <dangcongminh0203@gmail.com>
2022-08-10 15:57:20 +0700

    Part siamese

--- detect_image.py
+++ detect_image.py
@@ -20,7 +20,7 @@ from utils.general import (LOGGER, check_file, check_img_size, check_imshow, che
                        increment_path, non_max_suppression, print_args, scale_coords, strip_optimizer, xyxy2xywh)
 from utils.plots import Annotator, colors, save_one_box
 from utils.torch_utils import select_device, time_sync
-from train_identify import data_loader
+from crop_data import data_loader
 
 @torch.no_grad()
 def run(
--- main.py
+++ main.py
@@ -3,39 +3,40 @@ from PIL import Image
 import cv2
 import numpy as np
 
-from facenet_pytorch import InceptionResnetV1
 import torch
+from torchvision import transforms
 from torchvision import datasets
 from torch.utils.data import DataLoader
-from sklearn.svm import SVC
-from sklearn.multiclass import OneVsOneClassifier, OneVsRestClassifier
+import torch.nn.functional as F
+import torch.nn as nn
 
 import detect_image, detect_video
+
+ROOT = os.getcwd()
+
 
 
 class YOLOV5():
 	''' YOLOV5 load file detect_image từ file YoloV5 để tách và phân loại đối tượng
 		Ngoài ra crop từng object face trong ảnh để qua bước nhận diện 
 	'''
-	def __init__(self, source, path):
+	def __init__(self, source):
 		self.source = source # Đường dẫn của hình ảnh dùng để test
-		self.path = path # Đường dẫn hiện tại của file main.py
-
 
 	def take_object(self):
 		# Lấy các thông tin của từng object trong ảnh là center x,y,x,y và id class từ model YOLOV5
 		if self.source != 0:
 			output = detect_image.run(
-				weights=self.path + '/Model_Parameter/YOLOv5/weight_train.pt',
-				source= self.path + self.source, # file/dir/URL/glob, 0 for webcam
-				data=self.path + '/data/facemask.yaml',
+				weights=ROOT + '/Model_Parameter/YOLOv5/weight_train.pt',
+				source= ROOT + self.source, # file/dir/URL/glob, 0 for webcam
+				data=ROOT + '/data/facemask.yaml',
 				imgsz=(640, 640),  # inference size (height, width)
 				conf_thres=0.25,  # confidence threshold
 				iou_thres=0.45,  # NMS IOU threshold
 				max_det=1000,
 				save_crop=False,  # save cropped prediction boxes
 				view_img=False,  # maximum detections per image
-				project=self.path + '/Detect',  # save results to project/name
+				project=ROOT + '/Detect',  # save results to project/name
 				name='Face_Mask',  # save results to project/name
 				)
 
@@ -101,81 +102,131 @@ class YOLOV5():
 
 
 
-class FaceNet():
-	''' FaceNet dùng để train và nhận diện từng khuôn mặt dược tách ra bởi YoloV5
-	'''
-	def __init__(self, path):
-		self.resnet = InceptionResnetV1(pretrained='vggface2').eval() # initializing resnet for face img to embeding conversion
-		saved_data = torch.load(path) # loading data.pt file
-		self.embedding_list = saved_data[0] # getting embedding data
-		self.name_list = saved_data[1] # getting list of names
+class faceNet():
+	def __init__(self):
+		self.names_person = os.listdir(ROOT+ '/Person_data/') # List chứa các tên người nhận diện		
+		self.transformation = transforms.Compose([transforms.Resize((100,100)),
+									transforms.ToTensor()
+									]) # Định dạng ảnh trc khi qua siamese neural network
 
-
-	def face_match(self, face): # face= image of object face, type là np.array 
+	def face_match(self, img_crop, threshold=0.5): # face= image of object face, type là np.array 
 		# Tìm khoảng cách nhỏ nhất với tập dữ liệu và trả về kết quả
-		face = cv2.resize(face, (240, 240))
-		face = torch.from_numpy(face) #Change to tensor
-		face = face.unsqueeze(0) #Flatten
-		face = face.permute(0, 3, 1, 2).type(torch.float32) #Change size and type data
+		embeddings_list = [] # Chứa tất cả khoảng cách của face tới với toàn bộ dữ liệu
+		names_list = [] # Chứa tên tất cả các đối tượng
+		face1 = self.change_type_data(img_crop, PIL=False)
 
-		emb = self.resnet(face).detach().numpy() # detech is to make required gradient false
-		result = self.face_classify(emb)
+		for name in self.names_person:
+			all_name_images = os.listdir(ROOT+ '/Person_data/'+ name +'/')
 
-		return result
-		
+			for name_img in all_name_images:
+				path = ROOT+'/runs/crops/'+name+'/'+name_img
+				face2 = self.change_type_data(path)
 
-	def face_classify(self, emb, path= 'Model_Parameter/FaceNet/Face_data02.pt', C=[100]):
-		saved_data = torch.load(path) # Load data đã được trích feature 
-		X_train = np.array(saved_data[0]) # Convert to numpy
+				output1, output2 = net(face1, face2) # Embedding từ siamese neural network
+				euclidean_distance = F.pairwise_distance(output1, output2)
+				embeddings_list.append(euclidean_distance)
+				names_list.append(name)
 
-		y_train = [] # Khởi tạo list Label để train
-		label_list = {}
-		for i in range(len(X_train)):
-			label = saved_data[1][i][0]
-			if label not in label_list:
-				label_list[label] = saved_data[1][i][1]
-			y_train.append(int(label)) # Chuyển label là tên person về số
-		y_train = np.array(y_train) # Convert to numpy
-
-		for c in C:
-			model = SVC(kernel='linear', C=c, class_weight='balanced')
-			clf = OneVsOneClassifier(model) 
-			clf.fit(X_train, y_train)
-
-		result = clf.predict(emb)
-		print(label_list[result[0]])
-		return label_list[result[0]]
+		min_dist = min(embeddings_list)
+		if min_dist > threshold:
+			return 'No_name', -1
+		else:
+			return names_list[embeddings_list.index(min_dist)], min_dist.double()
 
 
-def identify_images(path_dict, path_images_test, path_faceNet, view_result, save_image):
+	def change_type_data(self, path, PIL=True):
+		''' Chuyển đổi dạng dữ liệu
+			Var: path đường dẫn hoặc ảnh có type = np.array
+				 PIL xác định ảnh là dạng PIL hay không
+		'''
+		if PIL:
+			img = Image.open(path) 
+			img = img.convert("L")
+		else:
+			img_convert = cv2.cvtColor(path, cv2.COLOR_BGR2GRAY) # Chuyển về ảnh xám
+			img = Image.fromarray(img_convert) # Chuyển về định dang PIL
+
+		img_trans = self.transformation(img) # Định dạng lại ảnh
+		img_numpy3 = img_trans.numpy()
+		img_numpy4 = np.array([img_numpy3]) # Tăng số chiều cho ảnh
+
+		return torch.from_numpy(img_numpy4)
+
+class SiameseNetwork(nn.Module):
+
+    def __init__(self):
+        super(SiameseNetwork, self).__init__()
+
+        # Setting up the Sequential of CNN Layers
+        self.cnn1 = nn.Sequential(
+            nn.Conv2d(1, 96, kernel_size=11,stride=4),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, stride=2),
+            
+            nn.Conv2d(96, 256, kernel_size=5, stride=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, stride=2),
+
+            nn.Conv2d(256, 384, kernel_size=3,stride=1),
+            nn.ReLU(inplace=True)
+        )
+
+        # Setting up the Fully Connected Layers
+        self.fc1 = nn.Sequential(
+            nn.Linear(384, 1024),
+            nn.ReLU(inplace=True),
+            
+            nn.Linear(1024, 256),
+            nn.ReLU(inplace=True),
+            
+            nn.Linear(256,2)
+        )
+        
+    def forward_once(self, x):
+        # This function will be called for both images
+        # It's output is used to determine the similiarity
+        output = self.cnn1(x)
+        output = output.view(output.size()[0], -1)
+        output = self.fc1(output)
+        return output
+
+    def forward(self, input1, input2):
+        # In this function we pass in both images and obtain both vectors
+        # which are returned
+        output1 = self.forward_once(input1)
+        output2 = self.forward_once(input2)
+
+        return output1, output2
+
+fNet = faceNet()
+yolo = YOLOV5(
+	source='/Images_test/' # Đường dẫn của hình ảnh dùng để test
+	)
+net = torch.load(ROOT + '/Model_Parameter/FaceNet/seamese_net.pth')
+
+
+
+def identify_images(view_result, save_image):
 	''' Liên kết các model lại với nhau để ra kết quả sau cùng
 		Hiển thị và lưu kết quả
 	'''
-	yolo = YOLOV5(
-		path=path_dict, # Đường dẫn của file main.py
-		source=path_images_test # Đường dẫn của hình ảnh dùng để test
-		) 
-	fNet = FaceNet(
-		path=path_faceNet # Đường dẫn file model đã train sẵn để nhận diện khuôn mặt
-		) 
-
-	# Detect Object
-	data = yolo.take_object() # Type data: dict
+	
+	data = yolo.take_object() # # Detect Object, type data return: dict
 	paths = data.keys() # Load path của tất cả hình ảnh test
 	colors = {
 		'Not Mask': (0, 0, 255),
 		'Correct': (0, 255, 0),
-		'Incorrect': (0, 255, 255)} # Màu sắc của từng class
+		'Incorrect': (0, 255, 255)} # Màu sắc của class
 
 	# Xóa data trong file nơi lưu hình ảnh đã nhận diện
 	if save_image:
 		name = 0
-		os.chdir(path_dict + '/Detect')
+		os.chdir(ROOT + '/Detect')
 		arr = os.listdir()
 		for char in arr:
 			if char[-4:] == '.jpg':
 				os.remove(char)
-		os.chdir(path_dict)
+		os.chdir(ROOT)
 
 	# Nhận diện với từng bức ảnh
 	for path in paths:
@@ -186,9 +237,9 @@ def identify_images(path_dict, path_images_test, path_faceNet, view_result, save
 
 		# Nhận diện từng đối tượng trên bức ảnh
 		for i in range(n):
-			img_crop = yolo.detect_box(data[path][i][1], im) # Ảnh khuôn mặt đã được cắt
-			result = fNet.face_match(img_crop) # Nhận kết quả nhận diện khuôn mặt
-			print(result)
+			img_crop = yolo.detect_box(data[path][i][1], im) # Ảnh khuôn mặt đã được cắt	
+			result, distance = fNet.face_match(img_crop) # Nhận kết quả nhận diện khuôn mặt
+			print('Object is {}, distance is {}'.format(result, distance))
 
 			# Open CV để hiển thị kết quả trên ảnh
 			if view_result or save_image:
@@ -200,8 +251,11 @@ def identify_images(path_dict, path_images_test, path_faceNet, view_result, save
 				else: size=im.shape[0]/500
 
 				name_class = data[path][i][0]
-				cv2.putText(im0, name_class, (x, y-10), fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=size, color=colors[name_class])
-				cv2.putText(im0, result, (x,y+h+int(h*0.15)), fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=size, color=colors[name_class])
+				if result == 'No_Name':
+					cv2.putText(im0, str(result), (x, y-10), fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=size, color=colors[name_class])
+				else: 
+					cv2.putText(im0, str(result)+', '+str(distance)[:6], (x, y-10), fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=size, color=colors[name_class])
+				cv2.putText(im0, name_class, (x,y+h+int(h*0.1)), fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=size, color=colors[name_class])		
 				cv2.rectangle(im0, (x,y), (x+w, y+h), colors[name_class], 5)
 
 		# Demo kết quả
@@ -211,28 +265,13 @@ def identify_images(path_dict, path_images_test, path_faceNet, view_result, save
 
 		# Lưu lại kết quả
 		if save_image:
-			cv2.imwrite(path_dict+'/Detect/'+str(name)+'.jpg', im0)
+			cv2.imwrite(ROOT+'/Detect/'+str(name)+'.jpg', im0)
 			name += 1
 
 
 
 if __name__ == '__main__':
-	curr_dict = os.getcwd()
 	identify_images(
-		path_dict= curr_dict, # Đường dẫn của file main.py
-		path_images_test='/Images_test/', # Đường dẫn của hình ảnh dùng để test
-		path_faceNet='Model_Parameter/FaceNet/Face_data02.pt', # Đường dẫn file model đã train sẵn để nhận diện khuôn mặt
 		view_result=False, # Demo kết quả
 		save_image=True # Lưu lại kết quả
 		)
-
-	# fNet = FaceNet(
-	# 	path='Model_Parameter/FaceNet/Face_data.pt') # Đường dẫn file model đã train sẵn để nhận diện khuôn mặt
-
-	# fNet.parameter_person()
-
-	# yolo = YOLOV5(
-	# 	path=curr_dict, # Đường dẫn của file main.py
-	# 	source=curr_dict + '/Images_test/') # Đường dẫn của hình ảnh dùng để test
-
-	# yolo.take_object()
